116ba3831a106aa0c31e51b7d6d5aaab4726d579
Solveen Gurung <solveengurung@gmail.com>
2022-02-22 20:34:23 +0545

    StaticInitializaiton

---
+++ src/HackerRank/StaticInitializationMain.java
@@ -0,0 +1,11 @@
+package HackerRank;
+
+public class StaticInitializationMain {
+
+    public static void main(String[] args) {
+        StaticInitialziation staticInitialziation = new StaticInitialziation();
+        staticInitialziation.si();
+
+    }
+}
+
